c3386632cc7742ee1cede25085f3ac06e040efd5
Muhannad <mo1981ot@gmail.com>
2023-03-10 03:05:49 +0100

    backgrounds

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer>
-            <h2>Copyright ©2021 Muhannad Osman</h2>
-        </footer>
-    )
-}
-export default Footer
+  return (
+    <footer>
+      <h2>Copyright ©2021 Muhannad Osman </h2>
+    </footer>
+  );
+};
+export default Footer;
